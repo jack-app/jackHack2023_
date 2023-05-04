4816a33c4f4a162998c48ead6386707be7ca86da
unknown <riku20020801@gmail.com>
2023-05-04 21:44:24 +0900

    あ

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const topAudio = new Audio("./audio/Lovers.mp3");
 const successAudio = new Audio("./audio/幸せな誓い.mp3");
 const failAudio = new Audio("./audio/さようなら.mp3");
 const alertAudio = new Audio("./audio/itemgetsea.mp3");
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
 let timer;
 let time = selectTime.value;
@@ -21,34 +21,34 @@ var initialOffset = 280;
 var i = 1;
 
 function playTopAudio() {
-    if (time == 280) {
-        topAudio.play();
-    }
+  if (time == 280) {
+    topAudio.play();
+  }
 }
 
 function stopTopAudio() {
-    topAudio.pause();
+  topAudio.pause();
 }
 
 function sendAlert() {
-    if (time == 290) {
-        alertAudio.play();
-    }
+  if (time == 290) {
+    alertAudio.play();
+  }
 }
 
 function Top() {
-    // 5秒経過したらモーダルを表示する
-    if (time == 295) {
-        modal.style.display = "block";
-    }
+  // 5秒経過したらモーダルを表示する
+  if (time == 295) {
+    modal.style.display = "block";
+  }
 }
 
 function updateTimerDisplay() {
-    const minutes = Math.floor(time / 60)
-        .toString()
-        .padStart(2, "0");
-    const seconds = (time % 60).toString().padStart(2, "0");
-    timeDisplay.textContent = `${minutes}:${seconds}`;
+  const minutes = Math.floor(time / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (time % 60).toString().padStart(2, "0");
+  timeDisplay.textContent = `${minutes}:${seconds}`;
 }
 
 function startTimer() {
@@ -67,10 +67,10 @@ function startTimer() {
 }
 
 function stopTimer() {
-    clearInterval(timer);
-    stopTopAudio();
-    successAudio.pause();
-    failAudio.pause();
+  clearInterval(timer);
+  stopTopAudio();
+  successAudio.pause();
+  failAudio.pause();
 }
 
 function resetTimer() {
@@ -87,13 +87,13 @@ timeDisplay.addEventListener("click", () => {
 });
 
 success.addEventListener("click", () => {
-    modal.style.display = "none";
-    successAudio.play();
+  modal.style.display = "none";
+  successAudio.play();
 });
 
 fail.addEventListener("click", () => {
-    modal.style.display = "none";
-    failAudio.play();
+  modal.style.display = "none";
+  failAudio.play();
 });
 
 selectTime.addEventListener("change", () => {
@@ -105,24 +105,22 @@ selectTime.addEventListener("change", () => {
 
 updateTimerDisplay();
 
-
 // canvas
 canvas.width = 700;
 canvas.height = 700;
-context.fillStyle = '#EAC7C7';
+context.fillStyle = "#EAC7C7";
 document.body.appendChild(canvas);
 
 // 円運動の中心座標。
 // 今回はcanvasの真ん中を中心に移動する。
 const centerX = 350;
-const centerY = 350;
+const centerY = 360;
 
 // 円を描画する中心からの距離。
 const distanceFromCenter = 300;
 
 // 表示する円のサイズ。
 const circleSize = 40;
-
 
 // 変化させていくパラメータ。
 // angleRadを増加させていき、
@@ -133,24 +131,24 @@ let angleRad = 90;
 
 // メインループ
 function loop(timestamp) {
-    // 描画内容を消去する。
-    context.clearRect(0, 0, canvas.width, canvas.height);
+  // 描画内容を消去する。
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // angleRadを1度ずつ変化させていく。
-    // 1度はMath.PI/180ラジアン。
-    angleRad -= 0.5 * Math.PI / 180;
+  // angleRadを1度ずつ変化させていく。
+  // 1度はMath.PI/180ラジアン。
+  angleRad -= (0.5 * Math.PI) / 180;
 
-    // ここで座標を変化させていく。
-    x = distanceFromCenter * Math.cos(angleRad) + centerX;
-    y = distanceFromCenter * Math.sin(angleRad) + centerY;
+  // ここで座標を変化させていく。
+  x = distanceFromCenter * Math.cos(angleRad) + centerX;
+  y = distanceFromCenter * Math.sin(angleRad) + centerY;
 
-    // 求めた座標に円を描画する。
-    context.beginPath();
-    context.arc(x, y, circleSize, 0, Math.PI * 2);
-    // context.rect(x, y, circleSize, circleSize);
-    context.fill();
+  // 求めた座標に円を描画する。
+  context.beginPath();
+  context.arc(x, y, circleSize, 0, Math.PI * 2);
+  // context.rect(x, y, circleSize, circleSize);
+  context.fill();
 
-    window.requestAnimationFrame(loop);
+  window.requestAnimationFrame(loop);
 }
 
 window.requestAnimationFrame(loop);
